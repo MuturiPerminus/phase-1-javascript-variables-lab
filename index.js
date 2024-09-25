0d95337675f7c1be7de2dea1e4e3612f6cedc384
Perminus Muturi <pthesh@hotmail.com>
2024-09-25 04:20:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+test/indexTest.js
+
+
+describe('index.js', function () {
+    //there's stuff in here, too
+});
+
+it('is set as Scuber', function() {
+    //test are here
+});
+
+it('is set as scuber', function() {
+    expect(companyName).to.equal('Scuber');
+});
+
+let companyName = 'Scuber';
